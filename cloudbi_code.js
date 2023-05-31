efaab4765ec9a710336dc1de78371f82f958ea0c
bulletz-break <91925965+bulletz-break@users.noreply.github.com>
2023-05-31 12:44:38 -0300

    Classe com as imagens

--- cloudbi_code.js
+++ cloudbi_code.js
@@ -1,30 +1,4 @@
-let pg_receita_image_carregar_on = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/CarregarON.png?raw=true";
-let pg_receita_image_carregar_off= "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/CarregarOFF.png?raw=true";
-let pg_receita_image_lavar_on = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/LavarON.png?raw=true";
-let pg_receita_image_lavar_off = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/LavarOFF.png?raw=true";
-let pg_receita_image_centrifugar_on = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/CentrifugarON.png?raw=true";
-let pg_receita_image_centrifugar_off = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/CentrifugarOFF.png?raw=true";
-let pg_receita_image_dreno_1_on = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/Dreno1.png?raw=true";
-let pg_receita_image_dreno_1_off = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/Dreno1.png?raw=true";
-let pg_receita_image_descarregar_on = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/DescaregarON.png?raw=true";
-let pg_receita_image_descarregar_off = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/DescaregarOFF.png?raw=true";
-let pg_receita_image_dreno_2_on = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/Dreno2ON.png?raw=true";
-let pg_receita_image_dreno_2_off = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/Dreno2.png?raw=true";
-let pg_receita_image_agua_1_on = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/Agua1.png?raw=true";
-let pg_receita_image_agua_1_off = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/Agua1OFF.png?raw=true";
-let pg_receita_image_nivel_baixo_on = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/nBaixoON.png?raw=true";
-let pg_receita_image_nivel_baixo_off = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/nBaixoOFF.png?raw=true";
-let pg_receita_image_nivel_medio_on = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/nMedioON.png?raw=true";
-let pg_receita_image_nivel_medio_off = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/nMedioOFF.png?raw=true";
-let pg_receita_image_nivel_alto_on = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/nAltoON.png?raw=true";
-let pg_receita_image_nivel_alto_off = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/nAltoOFF.png?raw=true";
-let pg_receita_image_agua_2_on = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/Agua2.png?raw=true";
-let pg_receita_image_agua_2_off = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/Agua2OFF.png?raw=true";
-let pg_receita_image_temperatura = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/4- Aquecer.png?raw=true";
-let pg_receita_image_produtos = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/5- Adicionar.png?raw=true";
-let pg_receita_image_time = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/6- Tempo.png?raw=true";
-let pg_receita_image_back = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/Back.png?raw=true";
-let pg_receita_image_logo = "https://github.com/bulletz-break/const_images/blob/692d650bbc645c302db6a07f90b2cf4067c646d6/LogoIKS.png?raw=true";
+let pg_receita_image = new WidgetImages();
 
 let options_geral       = {};   // JSON contendo os elementos gerais de lavagem
 let options_lavar       = {};   // JSON contendo os elementos da tela de Lavar
@@ -33,7 +7,6 @@ let options_produtos    = {};   // JSON contendo os elementos da tela dos produt
 let options_centrifugar = {};   // JSON contendo os elementos da tela de centrifugação
 let options_buttons     = {};   // JSON contendo os elementos dos botões de programação da receita
 let receita_object      = [];   // Objeto final da receita (contém a receita pronta)
-let receita_images      = {};   // JSON contendo as imagens
 let receita_json        = {};   // JSON final da receita
 let receita_data        = {
     "Carregar"      : false,
@@ -156,40 +129,8 @@ function pg_receita_get_elements_buttons() {
  * @brief Função para colocar as imagens nos elementos do widget
  */
 function pg_receita_set_images() {
-    receita_images["pg_receita_image_logo"]             = $("<img>").attr("src", pg_receita_image_logo);
-    receita_images["pg_receita_step_time_icon"]         = $("<img>").attr("src", pg_receita_image_time);
-
-    receita_images["pg_receita_image_carregar_off"]     = $("<img>").attr("src", pg_receita_image_carregar_off);
-
-    receita_images["pg_receita_image_lavar_off"]        = $("<img>").attr("src", pg_receita_image_lavar_off);
-
-    receita_images["pg_receita_image_centrifugar_off"]  = $("<img>").attr("src", pg_receita_image_centrifugar_off);
-
-    receita_images["pg_receita_image_dreno_1_off"]      = $("<img>").attr("src", pg_receita_image_dreno_1_off);
-
-    receita_images["pg_receita_image_descarregar_off"]  = $("<img>").attr("src", pg_receita_image_descarregar_off);
-
-    receita_images["pg_receita_image_dreno_2_off"]      = $("<img>").attr("src", pg_receita_image_dreno_2_off);
-
-    receita_images["pg_receita_image_agua_1_off"]       = $("<img>").attr("src", pg_receita_image_agua_1_off);
-
-    receita_images["pg_receita_image_nivel_baixo_off"]  = $("<img>").attr("src", pg_receita_image_nivel_baixo_off);
-
-    receita_images["pg_receita_image_nivel_medio_off"]  = $("<img>").attr("src", pg_receita_image_nivel_medio_off);
-
-    receita_images["pg_receita_image_nivel_alto_off"]   = $("<img>").attr("src", pg_receita_image_nivel_alto_off);
-
-    receita_images["pg_receita_image_agua_2_off"]       = $("<img>").attr("src", pg_receita_image_agua_2_off);
-
-    receita_images["pg_receita_image_temperatura"]      = $("<img>").attr("src", pg_receita_image_temperatura);
-    receita_images["pg_receita_image_produtos"]         = $("<img>").attr("src", pg_receita_image_produtos);
-    receita_images["pg_receita_image_back"]             = $("<img>").attr("src", pg_receita_image_back);
-    receita_images["pg_receita_image_dreno_1_off"]      = $("<img>").attr("src", pg_receita_image_dreno_1_off);
-
-    receita_images["pg_receita_image_dreno_2_off"]      = $("<img>").attr("src", pg_receita_image_dreno_2_off);
-
-    receita_images["pg_receita_image_back"]             = $("<img>").attr("src", pg_receita_image_back);
-    
+    $("#pg_receita_top_bar_logo").attr("src", pg_receita_image_logo);
+    $("#pg_receita_step_time_icon").attr("src", pg_receita_image_time);
 
     options_geral["Carregar"].attr("src", pg_receita_image_carregar_off);
     options_geral["Lavar"].attr("src", pg_receita_image_lavar_off);
