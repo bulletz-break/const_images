967a0424195c7aac05bd61269ab92e89cf36e440
Luiz Eurico <91925965+bulletz-break@users.noreply.github.com>
2024-02-02 16:25:37 -0300

    Update EditarReceitaOnline.js

--- EditarReceitaOnline.js
+++ EditarReceitaOnline.js
@@ -1504,7 +1504,7 @@ class EditarReceitaOnline {
      */
     load_receita_names() {        
         this.attributeService.getEntityAttributes(this.entity_id, "SHARED_SCOPE", ["receita_names"]).subscribe((attr) => {
-            this.receita_names  = (attr[0]["value"] == undefined ? false : attr[0]["value"]);
+            this.receita_names  = (attr[0] == undefined ? false : attr[0]);
         });
     }
 };
